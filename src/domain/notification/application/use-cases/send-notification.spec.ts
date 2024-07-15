18caa11f4b5f7c85615f1e8bfb050c056372285a
JonathanJoelVos <dev.jonathan.joel.vos@gmail.com>
2024-07-15 11:44:45 -0300

    Casos de uso de notificação

---
+++ src/domain/notification/application/use-cases/send-notification.spec.ts
@@ -0,0 +1,26 @@
+import { InMemoryNotificationsRepository } from "test/repositories/in-memory-notifications-repository";
+import { SendNotificationUseCase } from "./send-notification";
+import { UniqueEntityID } from "@/core/entities/unique-entity-id";
+
+let inMemoryNotificationsRepository: InMemoryNotificationsRepository;
+let sut: SendNotificationUseCase;
+describe("Send notification use case", () => {
+  beforeEach(() => {
+    inMemoryNotificationsRepository = new InMemoryNotificationsRepository();
+    sut = new SendNotificationUseCase(inMemoryNotificationsRepository);
+  });
+  it("should be able to send notification", async () => {
+    const result = await sut.execute({
+      content: "content",
+      recipientId: "id",
+      title: "title",
+    });
+
+    expect(result.isRight()).toBe(true);
+    if (result.isRight()) {
+      expect(inMemoryNotificationsRepository.items[0].id).toEqual(
+        result.value.notification.id
+      );
+    }
+  });
+});
