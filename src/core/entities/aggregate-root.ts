28a0f205482ebbd2430efcb10c19cc93c4736722
JonathanJoelVos <dev.jonathan.joel.vos@gmail.com>
2024-07-20 13:13:11 -0300

    Cria o evento de quando um comentário é adicionado a uma questão

--- src/core/entities/aggregate-root.ts
+++ src/core/entities/aggregate-root.ts
@@ -2,19 +2,4 @@ import { DomainEvent } from "../events/domain-event";
 import { DomainEvents } from "../events/domain-events";
 import { Entity } from "./entity";
 
-export class AggregateRoot<Props> extends Entity<Props> {
-  _domainEvents: DomainEvent[] = [];
-
-  get domainEvents() {
-    return this._domainEvents;
-  }
-
-  protected addDomainEvents(domainEvent: DomainEvent) {
-    this._domainEvents.push(domainEvent);
-    DomainEvents.markAggregateForDispatch(this);
-  }
-
-  public clearEvents() {
-    this._domainEvents = [];
-  }
-}
+export class AggregateRoot<Props> extends Entity<Props> {}
--- src/domain/forum/enterprise/entities/question-comment.ts
+++ src/domain/forum/enterprise/entities/question-comment.ts
@@ -2,6 +2,7 @@ import { Entity } from "@/core/entities/entity";
 import { UniqueEntityID } from "@/core/entities/unique-entity-id";
 import { Optional } from "@/core/types/optional";
 import { Comment, CommentProps } from "./comment";
+import { QuestionCommentCreatedEvent } from "../events/question-comment-created-event";
 
 export interface QuestionCommentProps extends CommentProps {
   questionId: UniqueEntityID;
@@ -12,13 +13,22 @@ export class QuestionComment extends Comment<QuestionCommentProps> {
     props: Optional<QuestionCommentProps, "createdAt">,
     id?: UniqueEntityID
   ) {
-    return new QuestionComment(
+    const questionComment = new QuestionComment(
       {
         ...props,
         createdAt: props.createdAt ?? new Date(),
       },
       id
     );
+    const isNewQuestionComment = !id;
+
+    if (isNewQuestionComment) {
+      questionComment.addDomainEvents(
+        new QuestionCommentCreatedEvent(questionComment)
+      );
+    }
+
+    return questionComment;
   }
 
   get questionId() {
